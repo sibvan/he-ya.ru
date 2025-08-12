0edd3882b37160b7b0d3bdf5ee8f7a52d7959348
sibvan <sibvan.dev@gmail.com>
2025-08-12 17:24:00 +0400

    404 page: add a button

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -4,6 +4,11 @@
       <img src="/404.svg" class="not-found__img" alt="404">
       <h1 class="h1 not-found__h1">Ошибка 404</h1>
       <p class="not-found__description">Страница не найдена</p>
+      <BaseButton class="not-found__btn" text="Вернуться на главную" to="/"></BaseButton>
     </div>
   </div>
 </template>
+
+<script setup>
+import BaseButton from "../components/ui/BaseButton.vue";
+</script>
